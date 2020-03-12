f5acf47f54873e7b9fd29228ffaf9cfcea8ed537
Nik Sofianos <nsofianos@hotmail.com>
2020-03-12 03:45:27 +0000

    print the requested resource into a file

---
+++ fetcher.js
@@ -0,0 +1,26 @@
+const request = require('request');
+const fs = require('fs');
+const userInput = process.argv.slice(2);
+const filePath = userInput[1];
+const URL = userInput[0];
+
+request(URL, (error, response, body) => {
+  if (error) {
+    console.log('\n',error);
+    process.exit();
+  }
+  fs.writeFile(filePath, body, (err) => {
+    if (err) {
+      throw err;
+    }
+    const fileSize = fs.statSync(filePath).size;
+    console.log(`Downloaded and saved ${fileSize} bytes to ${filePath}.`)
+  });
+});
+
+
+
+
+
+
+
